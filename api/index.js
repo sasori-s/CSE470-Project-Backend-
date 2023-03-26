fda2b39a248fa5e92c24d0792a5ed0f36b4fd91d
Sams <shams.iqbal@g.bracu.ac.bd>
2023-03-26 12:59:52 +0600

    Completed till Authentication (login/register)

--- api/index.js
+++ api/index.js
@@ -33,12 +33,7 @@ mongoose.connection.on('disconnected', ()=>{
 // })
 
 // middlewares
-app.use((req, res, next)=>{
-    // console.log("hi I am a middleware!")
-    // res.send("Hello from middleware")
-    console.log("Hii middleware")
-    next()
-})
+
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
@@ -46,6 +41,16 @@ app.use('/api/users', usersRoute)
 app.use('/api/hotels', hotelsRoute)
 app.use('/api/rooms', roomsRoute)
 
+app.use((err,req, res, next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success : false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
 
 
 app.listen(8800, ()=>{
